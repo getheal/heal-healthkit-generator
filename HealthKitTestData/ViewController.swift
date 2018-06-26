8760f47c10b8b413038c2f385f2c4d52faeb3b17
Ricky Kirkendall <rickykirkendall@openvpn-client.openvpn.heal.com>
2018-06-25 19:51:06 -0700

    Initial Commit

---
+++ HealthKitTestData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKitTestData
+//
+//  Created by Ricky Kirkendall on 6/25/18.
+//  Copyright © 2018 Ricky Kirkendall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
